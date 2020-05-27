9d9913ede7bc071c3261e2a765a6f751daca2dc5
Luiz Andrade <lcoandrade@gmail.com>
2020-05-27 20:41:34 -0300

    Clusters are treated correctly now.

--- diagrama_relacoes.py
+++ diagrama_relacoes.py
@@ -35,13 +35,6 @@ def getDataFrame(fileloc, ator, cor, grupo, de, relacionamento, para, tipo, bila
     df_relacionamentos = df_relacionamentos.loc[:,[de, relacionamento, para, tipo, bilateral]].dropna()
     return df_atores, df_relacionamentos
 
-#Cria o grafico
-g = Digraph(filename='diagrama_relacoes', engine='dot', format='png')
-g.attr(compound='true')
-g.attr(rankdir='BT')
-g.attr(dpi='600')
-g.attr(ratio = '0.5294')
-
 '''
 Faz os nós dos atores
 '''
@@ -51,7 +44,8 @@ def makeActorNodes(df):
     grupos = list(df.iloc[:,2])
     group_dict = {}
     for i in range(len(atores)):
-        g.node(atores[i],shape='circle',color=colors[cores[i]],style='filled', fontcolor=font_colors[cores[i]], fixedsize='true',width='1')
+        if atores[i] not in grupos:
+            g.node(atores[i],shape='circle',color=colors[cores[i]],style='filled', fontcolor=font_colors[cores[i]], fixedsize='true',width='1')
         if grupos[i] == '-': continue
         if grupos[i] not in group_dict.keys():
             group_dict[grupos[i]] = list()
@@ -73,7 +67,7 @@ def makeGroups(group_dict, atores):
 '''
 Cria os relacionamentos
 '''
-def makeRelationships(df):
+def makeRelationships(df, group_dict, atores):
     ports=['n','ne','e','se','s','sw','w','nw']
     #port_count_h = dict()
     #port_count_t = dict()
@@ -85,13 +79,46 @@ def makeRelationships(df):
     for i in range(len(des)):
         #pc_h = port_count_h.get(des[i],0)
         #pc_t = port_count_t.get(des[i],0)
-        g.edge(des[i], paras[i], dir=dirs[bilaterais[i]], label=relacionamentos[i], color=types[tipos[i]], fontcolor=types[tipos[i]],fontsize = '10', penwidth='1.0')#, headport=ports[pc_h%8], tailport=ports[pc_t%8])#, decorate='true')
+
+        #Parâmetros básicos
+        params = {'dir':dirs[bilaterais[i]], 'color':types[tipos[i]], 'fontcolor':types[tipos[i]], 'fontsize':'10', 'penwidth':'1.0', 'decorate':'false'}
+
+        #De e Para originais
+        de = des[i]
+        para = paras[i]
+
+        #Ajustando caso o relacionamento envolva clusters
+        if des[i] in group_dict.keys():
+            #Criando a chave ltail para o relacionamento com o cluster
+            params['ltail'] = 'cluster_'+des[i]
+            #Pegando um ator qualquer (o primeiro) dentro do cluster
+            de = atores[group_dict[des[i]][0]]
+        if paras[i] in group_dict.keys():
+            #Criando a chave lhead para o relacionamento com o cluster
+            params['lhead'] = 'cluster_'+paras[i]
+             #Pegando um ator qualquer (o primeiro) dentro do cluster
+            para = atores[group_dict[paras[i]][0]]
+        g.edge(de, para, label=relacionamentos[i], **params)#, headport=ports[pc_h%8], tailport=ports[pc_t%8])#, decorate='true')
         #port_count_h[des[i]] = pc_h+1
         #port_count_t[des[i]] = pc_t+1
 
-#Rodando o código
+'''
+Rodando o código
+'''
+#Cria o grafico
+g = Digraph(filename='diagrama_relacoes', engine='dot', format='png')
+g.attr(compound='true')
+g.attr(rankdir='LR')
+g.attr(dpi='600')
+g.attr(ratio = '0.5294')
+
+#Constroi os dataframes do excel
 df_atores, df_relacionamentos = getDataFrame(fileloc, ator, cor, grupo, de, relacionamento, para, tipo, bilateral)
+#Faz os nós dos atores
 group_dict, atores = makeActorNodes(df_atores)
-makeRelationships(df_relacionamentos)
+#Cria os clusters
 makeGroups(group_dict, atores)
+#Faz os relacionamentos
+makeRelationships(df_relacionamentos, group_dict, atores)
+#Abre o diagrama
 g.view()
